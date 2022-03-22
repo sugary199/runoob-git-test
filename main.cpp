4e4dc12964f55a26c148642da38248a1745f528e
sugary199 <906940958@qq.com>
2022-03-22 12:57:52 +0800

    添加了生成.h文件的脚本，未处理好describe的format
    Signed-off-by: sugary199 <906940958@qq.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+#include "version.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << _VERSION_ << std::endl;
     return 0;
 }
---
+++ version.h
@@ -0,0 +1,3 @@
+#ifndef _VERSION_
+#define _VERSION_ "version:  commit: 2022-03-22 11:32:15 build: 2022-03-22 12:56:57"
+#endif
