d1d3d7803ab0cc12518dccc711c1017ca57ac071
sugary199 <906940958@qq.com>
2022-03-22 13:02:21 +0800

    在build01.sh中处理好了describe，可以输出
    Signed-off-by: sugary199 <906940958@qq.com>

--- build01.sh
+++ build01.sh
@@ -2,7 +2,7 @@ commit_ts=`git log -1 --format="%ct"`
 commit_time=`date -d@$commit_ts +"%Y-%m-%d %H:%M:%S"`
 current_time=`date +"%Y-%m-%d %H:%M:%S"`
 git_version=`git log -1 --format="%h"`
-build_number=`git describe "`
+build_number=`git describe`
 sed  s/MYVERSION/"version: $build_number commit: $commit_time build: $current_time"/g version.h.tmp > version.h
 make clean
 make
--- version.h
+++ version.h
@@ -1,3 +1,3 @@
 #ifndef _VERSION_
-#define _VERSION_ "version:  commit: 2022-03-22 11:32:15 build: 2022-03-22 12:56:57"
+#define _VERSION_ "version: build01-5-g4e4dc12 commit: 2022-03-22 12:57:52 build: 2022-03-22 13:01:26"
 #endif
